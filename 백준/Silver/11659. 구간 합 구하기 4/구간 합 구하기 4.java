a1ca26afebd6c4e5f38220e36ae31da1d1722980
tjdqls1200 <dltjdqls1200@naver.com>
2023-02-22 16:42:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 572 ms, Memory: 56420 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -14,24 +14,19 @@ public class Main {
         final int N = Integer.parseInt(st.nextToken());
         final int M = Integer.parseInt(st.nextToken());
 
-        final int[] sumValues = new int[N];
+        final int[] sumValues = new int[N + 1];
 
         st = new StringTokenizer(br.readLine());
 
-        sumValues[0] = Integer.parseInt(st.nextToken());
-        for (int i = 1; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             sumValues[i] = sumValues[i - 1] + Integer.parseInt(st.nextToken());
         }
 
         for (int j = 0; j < M; j++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken()) - 2;
-            int end = Integer.parseInt(st.nextToken()) - 1;
+            int start = Integer.parseInt(st.nextToken()) - 1;
+            int end = Integer.parseInt(st.nextToken());
 
-            if (start < 0) {
-                sb.append(sumValues[end]).append("\n");
-                continue;
-            }
             sb.append(sumValues[end] - sumValues[start]).append("\n");
         }
 
