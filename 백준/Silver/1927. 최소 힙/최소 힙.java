d59a9e04f89d63862251137cc8d3f5515f4fd1d2
tjdqls1200 <dltjdqls1200@naver.com>
2023-04-20 19:54:10 +0900

    [Silver II] Title: 최소 힙, Time: 284 ms, Memory: 24680 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -2,25 +2,24 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        Queue<Integer> queue = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        final PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.naturalOrder());
-        final StringBuilder sb = new StringBuilder();
+
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
-
             if (num != 0) {
                 queue.offer(num);
                 continue;
             }
-            int outputNumber = 0;
+            int v = 0;
             if (!queue.isEmpty()) {
-                outputNumber = queue.poll();
+                v = queue.poll();
             }
-            sb.append(outputNumber).append("\n");
+            sb.append(v).append("\n");
         }
 
         System.out.println(sb);
