6d9699a70e0a241c622ef9ab53ad45f8518a52c5
tjdqls1200 <dltjdqls1200@naver.com>
2023-04-20 20:15:29 +0900

    [Silver I] Title: 회의실 배정, Time: 436 ms, Memory: 42248 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.java
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.java
@@ -2,37 +2,37 @@ import java.util.*;
 import java.io.*;
 
 class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
 
-        long[][] times = new long[n][2];
+        int[][] times = new int[n][2];
+        int endTime = 0;
+        int answer = 0;
+
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             times[i][0] = Integer.parseInt(st.nextToken());
             times[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(times, new Comparator<long[]>() {
+        Arrays.sort(times, new Comparator<int[]>() {
             @Override
-            public int compare(long[] o1, long[] o2) {
+            public int compare(int[] o1, int[] o2) {
                 if (o1[1] == o2[1]) {
-                    return (int) ((int) o1[0] - o2[0]);
+                    return o1[0] - o2[0];
                 }
-                return (int) ((int) o1[1] - o2[1]);
+                return o1[1] - o2[1];
             }
         });
 
-       long exitTime = 0;
-       int answer = 0;
-
-        for (long[] time : times) {
-            if (exitTime <= time[0]) {
-                exitTime = time[1];
+        for (int[] time : times) {
+            if (endTime <= time[0]) {
+                endTime = time[1];
                 ++answer;
             }
         }
+
         System.out.println(answer);
     }
 }
