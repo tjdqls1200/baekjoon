96acba2d0bcb97156628e2b4235ccb573a288892
tjdqls1200 <dltjdqls1200@naver.com>
2023-04-27 15:27:41 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1820 ms, Memory: 309188 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,12 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
-import java.util.function.BiFunction;
 
 class Main {
     public static void main(String[] args) throws IOException {
@@ -20,27 +16,23 @@ class Main {
             TreeMap<Integer, Integer> treeMap = new TreeMap<>();
             for (int j = 0; j < n; j++) {
                 st = new StringTokenizer(br.readLine());
-                String command = st.nextToken();
+                char command = st.nextToken().charAt(0);
                 int value = Integer.parseInt(st.nextToken());
-                if (command.startsWith("I")) {
+                if (command == 'I') {
                     treeMap.merge(value, 1, Integer::sum);
                     continue;
                 }
                 if (treeMap.isEmpty()) {
                     continue;
                 }
-                int key;
-                if (value == 1) {
-                    key = treeMap.lastKey();
-                } else {
-                    key = treeMap.firstKey();
-                }
+                int key = (value == 1) ? treeMap.lastKey() : treeMap.firstKey();
                 if (treeMap.get(key) == 1) {
                     treeMap.remove(key);
                 } else {
                     treeMap.merge(key, -1, Integer::sum);
                 }
             }
+            
             if (treeMap.isEmpty()) {
                 sb.append("EMPTY").append("\n");
             } else {
