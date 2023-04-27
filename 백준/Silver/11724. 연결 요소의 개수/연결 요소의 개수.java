5c9829237ec280046b671e5d138506b06c07aaf4
tjdqls1200 <dltjdqls1200@naver.com>
2023-04-27 16:35:03 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 145236 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,51 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 class Main {
-    static List<List<Integer>> lists = new ArrayList<>();
     static boolean[] ch;
+    static List<List<Integer>> lists;
+    
     public static void main(String[] args) throws IOException {
-        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
+        
         int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        lists = new ArrayList<>();
         ch = new boolean[n + 1];
-
+        
         for (int i = 0; i <= n; i++) {
             lists.add(new ArrayList<>());
         }
-        int m = Integer.parseInt(st.nextToken());
-
-        for (int j = 0; j < m; j++) {
+        
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             lists.get(a).add(b);
             lists.get(b).add(a);
         }
-
-        int count = 0;
+        
+        int answer = 0;
         for (int i = 1; i <= n; i++) {
             if (ch[i]) {
                 continue;
             }
+            ++answer;
             dfs(i);
-            ++count;
         }
-        System.out.println(count);
+        System.out.println(answer);
+    
     }
-
-    private static void dfs(int n) {
-        final List<Integer> list = lists.get(n);
-        for (int to : list) {
-            if (!ch[to]) {
-                ch[to] = true;
-                dfs(to);
+    
+    public static void dfs(int n) {
+        List<Integer> list = lists.get(n);
+        
+        for (int num : list) {
+            if (!ch[num]) {
+                ch[num] = true;
+                dfs(num);
             }
         }
     }
