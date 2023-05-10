e6acfc26649507929a85488dedf64da0d041b73e
tjdqls1200 <dltjdqls1200@naver.com>
2023-05-10 15:58:27 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 76 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 3];
+
+        dp[1] = 1;
+        dp[2] = 3;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % 10_007;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
