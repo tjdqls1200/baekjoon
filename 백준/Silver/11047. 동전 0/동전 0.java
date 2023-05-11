c15b2b2be9d522d7ca7bdc4d8648b44072b834d9
tjdqls1200 <dltjdqls1200@naver.com>
2023-05-11 16:53:40 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,29 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
+        int money = Integer.parseInt(st.nextToken());
+        int count = 0;
 
-        int[] moneyUnits = new int[n];
+        int[] coins = new int[n];
+
         for (int i = 0; i < n; i++) {
-            moneyUnits[i] = Integer.parseInt(br.readLine());
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        int count = 0;
-        int remainingMoney = k;
-
         for (int i = n - 1; i >= 0; i--) {
-            if (moneyUnits[i] <= remainingMoney) {
-                count += remainingMoney / moneyUnits[i];
-                remainingMoney %= moneyUnits[i];
+            if (money == 0) {
+                break;
             }
+
+            count += (money / coins[i]);
+            money %= coins[i];
         }
 
         System.out.println(count);
